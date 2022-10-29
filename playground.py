013442bf02faad20c132b90570d3c1ccd9fdc147
Morris Boers <morrisboers@gmail.com>
2022-10-29 23:53:13 +0200

    Added total sums of squares function with nan values not counting

--- playground.py
+++ playground.py
@@ -7,7 +7,8 @@ def main():
     schooldata = pd.read_csv('data/schooldata.csv')
     stats_object = Stats(schooldata)
     stats_object.set_groups('SCHOOL', 'PRE_ARITH')
-    print(stats_object.overall_average())
+    print(stats_object.overall_average)
+    print(stats_object.total_sum_of_squares)
 
 if __name__ == '__main__': 
     main()
--- src/statistics.py
+++ src/statistics.py
@@ -12,11 +12,17 @@ class Stats:
         self.data = data 
         self.n = len(data)
         self.c = len(data.iloc[0])
+        
+        self.target_name = ''
         self.grouping_name = ''
+
         self.m = 0 
         self.group_sizes = []
         self.groups = []
         self.groups_data = {}
+
+        self.overall_average = 0
+        self.total_sum_of_squares = 0
     
     def __set_group_data(self, current_group, previous, group_index): 
         self.groups.append(current_group)
@@ -28,6 +34,9 @@ class Stats:
 
 
     def set_groups(self, group_column_name, data_column_name):
+        # set target variable
+        self.target_name = data_column_name
+
         # sort data based on the grouping variable
         self.grouping_name = group_column_name
         self.data.sort_values(by=[self.grouping_name])
@@ -53,7 +62,10 @@ class Stats:
         # for the last group
         self.__set_group_data(current_group, previous, group_index)
 
-    def overall_average(self): 
+        self.overall_average = self.set_overall_average()
+        self.total_sum_of_squares = self.set_total_sum_of_squares()
+
+    def set_overall_average(self): 
         average = 0
         for key in self.groups_data: 
             # if all values are nan, it is not counted for the average
@@ -87,5 +99,10 @@ class Stats:
 
         return sum, missing_count
 
-    def sum_of_squares(self): 
-        return "lol"
+    def set_total_sum_of_squares(self): 
+        tss = 0 
+        for i in range(self.n): 
+            print(self.data.iloc[i][self.target_name])
+            if not math.isnan(self.data.iloc[i][self.target_name]): 
+                tss += (self.data.iloc[i][self.target_name] - self.overall_average) ** 2
+        return tss 
